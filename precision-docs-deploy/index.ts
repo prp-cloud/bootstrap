4740433dc0adcd89573afc35e5c1242ca7d72d72
Abraham Guo <aguo@precisionplanting.com>
2023-05-26 13:34:38 -0500

    finish script

--- precision-docs-deploy/index.ts
+++ precision-docs-deploy/index.ts
@@ -1,14 +1,14 @@
 import { readdir, readFile, writeFile } from 'fs/promises';
 
-const recurse = async (dir: string) =>
-	await Promise.all(
+const recurse = async (dir: string): Promise<unknown> =>
+	Promise.all(
 		(await readdir(dir, { withFileTypes: true }))
 			.map(async dirent => {
 				const path = `${dir}/${dirent.name}`;
 				if (!dirent.isFile()) return recurse(path);
 				if (!dirent.name.endsWith(`.html`)) return;
 				const contents = `${await readFile(path)}`;
-				const replaced = contents.replaceAll(`src="/docs`, `src="/bootstrap/docs`);
+				const replaced = contents.replaceAll(/(?<=(href|src)=")(?=\/docs)/g, `/bootstrap`);
 				if (contents != replaced) return writeFile(path, replaced);
 			})
 	);
