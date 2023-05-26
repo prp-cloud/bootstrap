5271d99fa1f5516df28765ae312f948427c5d02c
Abraham Guo <aguo@precisionplanting.com>
2023-05-26 12:03:21 -0500

    WIP

--- precision-docs-deploy/index.ts
+++ precision-docs-deploy/index.ts
@@ -1,7 +1,7 @@
 import { readdir, readFile } from 'fs/promises';
 
 const matches = await Promise.all(
-	(await readdir(`_site`, { withFileTypes: true, recursive: true }))
+	(await readdir(`_site`, { withFileTypes: true }))
 		.filter(dirent => dirent.isFile() && dirent.name.endsWith(`.html`))
 		.map(async dirent =>
 			[...`${await readFile(`${dirent.path}/${dirent.name}`)}`.matchAll(/[^ ]+(?=docs)/g)]
