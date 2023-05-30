aeeb1fb85556fd4c3809ee6eb2e1763cd2f1c9c6
Abraham Guo <aguo@precisionplanting.com>
2023-05-30 07:37:55 -0500

    green favicon

---
+++ site/assets/js/precision-favicon.js
@@ -0,0 +1,18 @@
+(() => {
+  'use strict'
+  for (const link of document.querySelectorAll('[rel*=icon]:not([rel=mask-icon])')) {
+    const icon = Object.assign(
+      new Image(),
+      {
+        async onload() {
+          const canvas = new OffscreenCanvas(...['width', 'height'].map(wh => icon[wh]))
+          const ctx = canvas.getContext('2d')
+          ctx.filter = 'hue-rotate(220deg)'
+          ctx.drawImage(icon, 0, 0)
+          link.href = (URL.createObjectURL(await canvas.convertToBlob()))
+        },
+        src: link.href
+      }
+    )
+  }
+})()
