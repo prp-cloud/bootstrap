57b2174198c33d747bc01611a0fde25b590d6302
Abraham Guo <aguo@precisionplanting.com>
2023-07-11 17:49:55 -0500

    remove ga onclicks

--- precision-docs-deploy/index.ts
+++ precision-docs-deploy/index.ts
@@ -8,7 +8,9 @@ const recurse = async (dir: string): Promise<unknown> =>
 				if (!dirent.isFile()) return recurse(path);
 				if (!dirent.name.endsWith(`.html`)) return;
 				const contents = `${await readFile(path)}`;
-				const replaced = contents.replaceAll(/(?<=(href|src(set)?)=")(?=\/docs|\/?")/g, `/bootstrap`);
+				const replaced = contents
+					.replaceAll(/(?<=(href|src(set)?)=")(?=\/docs|\/?")/g, `/bootstrap`)
+					.replaceAll(/ga\(.+?\)/g, match => { console.log(match); return ``; });
 				if (contents != replaced) return writeFile(path, replaced);
 			})
 	);
