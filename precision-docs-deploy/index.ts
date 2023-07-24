9c1b731b249d0a06aeccf5bb9edf1f95309ab597
Abraham Guo <aguo@precisionplanting.com>
2023-07-24 10:06:43 -0500

    fix srcset

--- precision-docs-deploy/index.ts
+++ precision-docs-deploy/index.ts
@@ -1,12 +1,15 @@
-import { readdir, writeFile } from 'fs/promises';
+import { readdir } from 'fs/promises';
 import { JSDOM } from 'jsdom';
 
 const now = Date.now();
 const { entries } = Object;
-const transforms: { attr: string, fn?: (val: string, node: Node) => string, results: Record<string, number> }[] = [
+const transforms = [
 	...[`href`, `src`, `srcset`].map(attr => ({
 		attr,
-		fn: (val: string) => val.split(/,\s*/).map(src => (/^(?=\/docs|\/$)/.test(src) ? `/bootstrap` : ``) + src).join(`,`)
+		fn: (val: string) =>
+			val.startsWith(`data:`)
+				? val
+				: val.split(/,\s*/).map(src => (/^(?=\/docs|\/$)/.test(src) ? `/bootstrap` : ``) + src).join(`, `)
 	})),
 	{ attr: `onclick` },
 	...entries({ LINK: `href`, SCRIPT: `src` }).map(([nodeName, attr]) => ({
@@ -16,7 +19,7 @@ const transforms: { attr: string, fn?: (val: string, node: Node) => string, resu
 			(nodeName == node.nodeName && !val.startsWith(`https://`) && node.rel != `canonical` ? `?${now}` : ``)
 	}))
 ]
-	.map(transform => ({ ...transform, results: {} }));
+	.map(transform => ({ ...transform, results: <Record<string, number>>{} }));
 
 const recurse = async (dir: string): Promise<unknown> =>
 	Promise.all(
@@ -26,10 +29,10 @@ const recurse = async (dir: string): Promise<unknown> =>
 			if (!dirent.name.endsWith(`.html`)) return;
 			const jsdom = await JSDOM.fromFile(path);
 			for (const node of jsdom.window.document.querySelectorAll(`*`))
-				for (const { attr, fn, results } of transforms) {
+				for (const { attr, results, ...transform } of transforms) {
 					const val = node.getAttribute(attr);
 					if (!val) continue;
-					const transformed = fn?.(val, node);
+					const transformed = `fn` in transform && transform.fn(val, node);
 					if (transformed == val) continue;
 					if (transformed) node.setAttribute(attr, transformed);
 					else node.removeAttribute(attr);
@@ -37,7 +40,7 @@ const recurse = async (dir: string): Promise<unknown> =>
 					const key = `${val} => ${transformed}`;
 					results[key] = (results[key] ?? 0) + 1;
 				}
-			return writeFile(path, jsdom.serialize());
+			// return writeFile(path, jsdom.serialize());
 		})
 	);
 
